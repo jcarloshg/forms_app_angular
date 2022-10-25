6c518eebc1414b9fa6909e95299e79e777b7d5b6
jcarloshg <carlosj12336@gmail.com>
2022-10-25 00:17:22 -0500

    ADD form template

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  texto: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.css'],
+})
+export class SidemenuComponent {
+
+  menuItemsTemplate: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      route: './template/basicos',
+    },
+    {
+      texto: 'Dinamicos',
+      route: './template/dinamicos',
+    },
+    {
+      texto: 'Switches',
+      route: './template/switches',
+    },
+  ];
+
+  menuItemsReactive: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      route: './reactive/basicos',
+    },
+    {
+      texto: 'Dinamicos',
+      route: './reactive/dinamicos',
+    },
+    {
+      texto: 'Switches',
+      route: './reactive/switches',
+    },
+  ];
+}
