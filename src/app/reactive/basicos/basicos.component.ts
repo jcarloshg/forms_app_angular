73826c263792baa9a3cd7ebba6ee04b3fa0d12ee
jcarloshg <carlosj12336@gmail.com>
2022-10-26 18:49:37 -0500

    add logic to dinamicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080ti'),
+  //   precio: new FormControl(0),
+  //   existencias: new FormControl(5),
+  // });
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [null, [Validators.required, Validators.minLength(3)]],
+    precio: [null, [Validators.required, Validators.min(0)]],
+    existencias: [null, [Validators.required, Validators.min(1)]],
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    // * esto produce error
+    // this.miFormulario.setValue({
+    // nombre: 'RTX 4080ti',
+    // precio: 1600,
+    // // existencias: 10,
+    // });
+
+    this.miFormulario.reset({
+      nombre: 'RTX 4080ti',
+      precio: 1600,
+    });
+  }
+
+  campoValido(fieldName: string): boolean {
+
+    const filedTouched = this.miFormulario.controls[fieldName].touched;
+    const filedHaveErrors = this.miFormulario.controls[fieldName].errors;
+
+    const fieldIsValid = filedTouched && filedHaveErrors;
+
+    return (fieldIsValid == null || fieldIsValid == false)
+      ? false
+      : true;
+
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario);
+    this.miFormulario.reset();
   }
 
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [null, [Validators.required, Validators.minLength(3)]],
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  checkField(nameField: string): boolean {
+
+    const fieldTouched = this.miFormulario.controls[nameField].touched;
+    const fieldHaveErrors = this.miFormulario.controls[nameField].errors;
+
+    const isValidField = fieldTouched && fieldHaveErrors;
+
+    return (isValidField == false || isValidField == null) ? false : true;
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario);
+    this.miFormulario.reset();
   }
 
 }
