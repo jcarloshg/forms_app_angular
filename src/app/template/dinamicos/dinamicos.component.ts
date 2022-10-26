e599ff3b4f7899cec4c029adaf6da8def92d7ac0
jcarloshg <carlosj12336@gmail.com>
2022-10-26 00:02:01 -0500

    point save

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[],
+}
+
+interface Favorito {
+  id: number,
+  name: string,
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Jose Carlos',
+    favoritos: [
+      {
+        id: 1,
+        name: 'Blinx',
+      },
+      {
+        id: 2,
+        name: 'Halo',
+      },
+    ]
+  }
+
+  agregarNuevoJuego() {
+    const nuevoJuego: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      name: this.nuevoJuego,
+    }
+
+    this.persona.favoritos.push({ ...nuevoJuego });
+    this.nuevoJuego = '';
   }
 
   guardar() {
-    console.log('salkdjf');
+    console.log(`[persona] -> `, this.persona);
+  }
 
+  elliminarFavorito(index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
 }
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SwitchesComponent {
+  persona = {
+    genero: ''
   }
-
 }
